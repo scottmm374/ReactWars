0fa0362859c2f2372bc40d38decc78d0ab7b3d09
Michelle Scott <scottmm374@gmail.com>
2019-09-03 14:04:50 -0400

    Copied files over from Org so I can deploy site on Netlify

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,56 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
+import StarWarsCard from './components/StarWarsCard';
+import axios from 'axios';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [speciesData, setSpeciesData] = useState([]);
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  useEffect(() => {
+    axios
+      .get('https://swapi.co/api/species/')
+      .then(response =>
+        response.data.results.map(species => ({
+          name: species.name,
+          height: species.average_height,
+          eyeColor: species.eye_colors,
+          hairColor: species.hair_colors,
+          skinColor: species.skin_colors,
+          classification: species.classification,
+          designation: species.designation,
+          lifeSpan: species.average_lifespan,
+          language: species.language,
+          homeWorld: species.homeworld,
+        }))
+      )
+      .then(data => {
+        setSpeciesData(data);
+      });
+
+  }, []);
+
+
+  console.log("speciesData", speciesData)
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      {speciesData.map(item => (
+        <StarWarsCard
+        key={item.name}
+          name={item.name}
+          height={item.height}
+          eyeColor={item.eyeColor}
+          hairColor={item.hairColor}
+          skinColor={item.skinColor}
+          classification={item.classification}
+          designation={item.designation}
+          lifeSpan={item.lifeSpan}
+          language={item.language}
+          homeWorld={item.homeWorld}
+        />
+      ))};
     </div>
   );
 }
